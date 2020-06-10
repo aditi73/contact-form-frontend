ad4f8df16510b2dfe20d747559a639775e07491e
Aditi Thakur <aditit@hashtagloyalty.com>
2020-06-11 02:09:07 +0530

    add routing for contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'contacts', component: ContactComponent },
+  { path: '',   redirectTo: '/contacts', pathMatch: 'full' },
+  { path: '**', component: ContactComponent },
+  // { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
