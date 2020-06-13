0a561f5d7c9123bc4aac716ae361bf46db233eef
Aditi Thakur <aditit@hashtagloyalty.com>
2020-06-13 22:06:10 +0530

    dynamic render form fields | add language selector

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import {TranslateService} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-contact',
@@ -8,8 +9,20 @@ import { FormBuilder, Validators } from '@angular/forms';
 })
 export class ContactComponent implements OnInit {
   contactForm:any;
+  formData: any;
+  companyData: any;
   formSubmitAttempt: boolean = false;
-  constructor(private formBuilder: FormBuilder) { }
+  constructor(private formBuilder: FormBuilder, public translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
+    if (localStorage.getItem('locale')) {
+      const browserLang = localStorage.getItem('locale');
+      translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+    } else {
+      localStorage.setItem('locale', 'en');
+      translate.setDefaultLang('en');
+    }
+  }
 
   ngOnInit(): void {
     this.contactForm  =  this.formBuilder.group({
@@ -19,8 +32,23 @@ export class ContactComponent implements OnInit {
       phone:[''],
       message: ['',[Validators.required]]
     });
+    // form fields along with their details
+    this.formData = [
+                      { fieldName: 'first_name', label: 'Name', class: 'wrap-input100 validate-input rs1-wrap-input100', placeholder: 'Fname', ValidateError: 'FnameError', id: 'first-name' },
+                      { fieldName: 'last_name', label: '', class: 'wrap-input100 validate-input rs2-wrap-input100', placeholder: 'Lname', ValidateError: 'LnameError', id: 'last-name' },
+                      { fieldName: 'email', label: 'Email', class: 'wrap-input100 validate-input', placeholder: 'EmailEg', ValidateError: 'EmailError', id: 'email' },
+                      { fieldName: 'phone', label: 'Phone', class: 'wrap-input100 validate-input', placeholder: 'PhoneEg', ValidateError: '', id: 'phone' },
+                      { fieldName: 'message', label: 'Message', class: 'wrap-input100 validate-input', placeholder: 'WriteMessage', ValidateError: 'MessageError', id: 'message' }
+                    ];
+    //  static company details
+    this.companyData = [
+                        { class: 'lnr lnr-map-marker', type: 'Address', value: 'Mada Center 8th floor, 379 Hudson St, New York, NY 10018 US', valueClass: 'txt2' },
+                        { class: 'lnr lnr-phone-handset', type: 'Talk', value: '+1 800 1236879', valueClass: 'txt3' },
+                        { class: 'lnr lnr-envelope', type: 'Support', value: 'contact@example.com', valueClass: 'txt3' }
+                      ];
   }
 
+  // method to submit contact details
   saveContact(){
     this.formSubmitAttempt = true;
     if(this.contactForm.dirty && this.contactForm.valid){
@@ -28,6 +56,7 @@ export class ContactComponent implements OnInit {
     }
   }
 
+  // check fields validity
   isFieldValid(field: string) {
     return (!this.contactForm.get(field).valid && this.contactForm.get(field).touched) ||
       (this.contactForm.get(field).untouched && this.formSubmitAttempt);
@@ -37,6 +66,12 @@ export class ContactComponent implements OnInit {
     return {
       'alert-validate': this.isFieldValid(field)
     };
+  }
+
+  // change language
+  changeLang(language: string) {
+    localStorage.setItem('locale', language);
+    this.translate.use(language);
   }
 
 }
